863a39a9e5ed34378133f65ddf1ee9297b458ad3
Skye Haik <shaik@nicolawealth.com>
2025-02-25 11:16:04 -0800

    delete a test temporarily to test codecov badge feature

--- src/defer_until.test.ts
+++ src/defer_until.test.ts
@@ -2,20 +2,7 @@ import {deferUntilFactory} from "./defer_until";
 import sinon from "sinon";
 
 describe("defer_until tests", () => {
-  it("basic functionality", async () => {
-    const deferralsT1 = deferUntilFactory();
-    const func1 = sinon.stub();
-    const func2 = sinon.stub();
-    const func3 = sinon.stub();
-    deferralsT1.defer(func1);
-    deferralsT1.defer(func2);
-    deferralsT1.defer(func3);
-    deferralsT1.later();
-    sinon.assert.calledOnce(func1);
-    sinon.assert.calledOnce(func2);
-    sinon.assert.calledOnce(func3);
-    sinon.assert.callOrder(func3, func2, func1);
-  });
+
   it("later empties deferrals", async () => {
     const deferralsT2 = deferUntilFactory();
     const func1 = sinon.stub();
